51dbaad18237be24d095b15e35081fea9c425854
JH Byun <61155854+Jiheebyun@users.noreply.github.com>
2023-08-02 10:53:49 +0900

    Time: 52 ms (83.62%), Space: 41.5 MB (82.95%) - LeetHub

--- 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -32,5 +32,9 @@ var expect = function(val) {
 1.  클래스가 아닌데, "."을 이용하여 함수를 콜하고 있다 
     어떻게 하면, "."을 이용하여 함수를 콜할수있을까?
     -> 함수안에 객체를 선언하여 객체 안에 함수를 넣고, return 값으로 객체를 반환.
+    
+2. 삼항연산자를 사용해서 throw new Error() 왜 사용할수 없을까?
+// return val === compareVal ? true : throw new Error("Not Equal");
+// return val === compareVal ? true : throw new Error("Equal");
 
 */
